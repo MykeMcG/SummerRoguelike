61f71ff2ab28bf470b82f07b938ca1524fa5d55f
Mike McGivern <mike@bitsandpieces.tech>
2017-06-28 06:38:57 -0300

    Rename player.py -> entity.py
    Renamed player.py to entity.py and refactored it.
    Misc. refactoring in firstrl.py

---
+++ src/entity.py
@@ -0,0 +1,24 @@
+import libtcodpy as libtcod
+
+class entity:
+    #A generic object [player, monster, item, stairs, etc.]
+    #It is always represented by a character on screen.
+    def __init__(self, x, y, char, color, background):
+        self.x          = x
+        self.y          = y
+        self.char       = char
+        self.color      = color
+        self.background = background
+
+    def move(self, delta_x, delta_y):
+        self.x += delta_x
+        self.y += delta_y
+
+    def draw(self, console):
+        #set the color and draw the character representing the object
+        libtcod.console_set_default_foreground(console, self.color)
+        libtcod.console_put_char(console, self.x, self.y, self.char, self.background)
+
+    def clear(self, console):
+        #erase the character representing the object
+        libtcod.console_put_char(console, self.x, self.y, ' ', libtcod.BKGND_NONE)
--- src/firstrl.py
+++ src/firstrl.py
@@ -1,46 +1,47 @@
 import libtcodpy as libtcod
-from player import player
+from entity import entity
 
 SCREEN_WIDTH  = 80
 SCREEN_HEIGHT = 50
 playerx = SCREEN_WIDTH//2
 playery = SCREEN_HEIGHT//2
+objects = None
 
-def handle_keys(plr):
+def handle_keys(player):
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #Alt+Enter: toggle fullscreen
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
     elif key.vk == libtcod.KEY_ESCAPE:
         return True  #exit game
-    global playerx, playery
     #movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        plr.pos_y -= 1
+        player.move( 0,  1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        plr.pos_y += 1
+        player.move( 0, -1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        plr.pos_x -= 1
+        player.move(-1,  0)
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        plr.pos_x += 1
+        player.move( 1,  0)
 
 def main():
     libtcod.console_set_custom_font('arial12x12.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Wrath of Exuleb', False)
     con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-    plr = player(playerx, playery, libtcod.BKGND_NONE)
-    #draw the initial screen
-    plr.draw(con)
-    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
-    libtcod.console_flush()
+    player = entity(playerx, playery, '@', libtcod.white, libtcod.BKGND_NONE)
+    npc = entity(playerx, playery + 5, '@', libtcod.green, libtcod.BKGND_NONE)
+    objects = [player, npc]
     while not libtcod.console_is_window_closed():
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        exit = handle_keys(plr)
-        plr.draw(con)
-        if exit:
-            break
+        for object in objects:
+            object.clear(con)
+        for object in objects:
+            object.draw(con)
         libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
         libtcod.console_flush()
+        libtcod.console_set_default_foreground(con, libtcod.white)
+        exit = handle_keys(player)
+        if exit:
+            break
 
 if __name__ == '__main__':
     main()
