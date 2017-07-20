09c67a4eb13d42e0cae9c582bf475a74df053856
MykeMcG <myke.mcg@gmail.com>
2017-07-20 14:35:25 -0300

    Refactor firstrl
    Cleaned up firstrl according to the Python style guide.

--- src/firstrl.py
+++ src/firstrl.py
@@ -1,109 +1,108 @@
 import libtcodpy as libtcod
-from entity import Entity
 from player import Player
 from bspmapgenerator import BspMapGenerator
 
-#Constants
-SCREEN_WIDTH    = 80
-SCREEN_HEIGHT   = 50
-MAP_WIDTH       = 80
-MAP_HEIGHT      = 45
-ROOM_MIN_SIZE   = 6
-MAX_ROOMS       = 50
-FOV_ALGORITHM   = 1
+# Constants
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 50
+FOV_ALGORITHM = 1
 FOV_LIGHT_WALLS = True
-TORCH_RADIUS    = 10
+TORCH_RADIUS = 10
 
-#Debug Flags
+# Debug Flags
 DEBUG = True
-debug_show_whole_map = False
+DebugShowWholeMap = False
 
-#Colors
-color_dark_wall    = libtcod.Color( 50,  50, 150)
-color_dark_ground  = libtcod.Color(  0,   0, 100)
-color_light_wall   = libtcod.Color(204, 204, 204)
-color_light_ground = libtcod.Color( 33,  33,  33)
+# Colors
+COLOR_DARK_WALL = libtcod.Color(50, 50, 150)
+COLOR_DARK_GROUND = libtcod.Color(0, 0, 100)
+COLOR_LIGHT_WALL = libtcod.Color(204, 204, 204)
+COLOR_LIGHT_GROUND = libtcod.Color(33, 33, 33)
 
-#Map Options
+# Map Options
 BSP_RECURSION_DEPTH = 30
-BSP_FULL_ROOMS      = False
+BSP_FULL_ROOMS = False
 
-#Game Variables
-playerx           = None
-playery           = None
-objects           = None
-max_room_monsters = 2
-terrain_map       = None
-fov_recompute     = True
-game_state        = 'playing'
-player_action     = None
+# Game Variables
+PlayerX = None
+PlayerY = None
+Objects = None
+MAX_ROOM_MONSTERS = 2
+MapTiles = None
+FovRecompute = True
+GameState = 'playing'
+PlayerAction = None
 
 
 def handle_keys(player):
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
-        # Alt+Enter: toggle fullscreen
+        # Alt+Enter: toggle full-screen
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
     elif key.vk == libtcod.KEY_ESCAPE:
         return 'exit'  # exit game
     elif key.vk == libtcod.KEY_F1 and DEBUG:
-        global debug_show_whole_map
-        debug_show_whole_map = not debug_show_whole_map #Toggle debug_show_whole_map
-    if game_state == 'playing':
+        global DebugShowWholeMap
+        DebugShowWholeMap = not DebugShowWholeMap  # Toggle DebugShowWholeMap
+    if GameState == 'playing':
         # movement keys
-        global fov_recompute
+        global FovRecompute
         if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-            player.move_or_attack(0, -1, terrain_map, objects)
-            fov_recompute = True
+            player.move_or_attack(0, -1, MapTiles, Objects)
+            FovRecompute = True
         elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-            player.move_or_attack(0, 1, terrain_map, objects)
-            fov_recompute = True
+            player.move_or_attack(0, 1, MapTiles, Objects)
+            FovRecompute = True
         elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-            player.move_or_attack(-1, 0, terrain_map, objects)
-            fov_recompute = True
+            player.move_or_attack(-1, 0, MapTiles, Objects)
+            FovRecompute = True
         elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-            player.move_or_attack(1, 0, terrain_map, objects)
-            fov_recompute = True
+            player.move_or_attack(1, 0, MapTiles, Objects)
+            FovRecompute = True
         else:
             return 'didnt-take-turn'
 
 
-def render_wall(con, terrain_map, x, y, color, background):
+def render_wall(con, x, y, color):
     #TODO: Think of a better way to do this
     #TODO: Improve outside walls
     if y + 1 >= MAP_HEIGHT:
         north = False
     else:
-        north = terrain_map[x][y + 1].blocked
+        north = MapTiles[x][y + 1].blocked
     if y - 1 < 0:
         south = False
     else:
-        south = terrain_map[x][y - 1].blocked
+        south = MapTiles[x][y - 1].blocked
     if x + 1 >= MAP_WIDTH:
         west = False
     else:
-        west  = terrain_map[x + 1][y].blocked
+        west  = MapTiles[x + 1][y].blocked
     if x - 1 < 0:
         east = False
     else:
-        east = terrain_map[x - 1][y].blocked
-    if       north and     south and     east and     west:
+        east = MapTiles[x - 1][y].blocked
+    if north and south and east and west:
         wall_char = libtcod.CHAR_DCROSS
-    elif     north and not south and     east and     west:
+    elif north and not south and east and west:
         wall_char = libtcod.CHAR_DTEES
-    elif not north and     south and     east and     west:
+    elif not north and south and east and west:
         wall_char = libtcod.CHAR_DTEEN
-    elif     north and     south and     east and not west:
+    elif north and south and east and not west:
         wall_char = libtcod.CHAR_DTEEW
-    elif     north and     south and not east and     west:
+    elif north and south and not east and west:
         wall_char = libtcod.CHAR_DTEEE
-    elif     north and not south and     east and not west:
+    elif north and not south and east and not west:
         wall_char = libtcod.CHAR_DNE
-    elif     north and not south and not east and     west:
+    elif     north and not south and not east and west:
         wall_char = libtcod.CHAR_DNW
-    elif not north and     south and     east and not west:
+    elif not north and south and  east and not west:
         wall_char = libtcod.CHAR_DSE
-    elif not north and     south and not east and     west:
+    elif not north and south and not east and west:
         wall_char = libtcod.CHAR_DSW
     elif north and south:
         wall_char = libtcod.CHAR_DVLINE
@@ -115,67 +114,67 @@ def render_wall(con, terrain_map, x, y, color, background):
     libtcod.console_set_char(con, x, y, wall_char)
 
 
-def render_all(con, terrain_map, fov_map, objects):
+def render_all(con, fov_map):
     for y in range(MAP_HEIGHT):
         for x in range(MAP_WIDTH):
-            if debug_show_whole_map:
+            if DebugShowWholeMap:
                 visible = True
             else:
                 visible = libtcod.map_is_in_fov(fov_map, x, y)
-            wall = terrain_map[x][y].block_sight
+            wall = MapTiles[x][y].block_sight
             if not visible:
-                if terrain_map[x][y].explored or debug_show_whole_map:
+                if MapTiles[x][y].explored or DebugShowWholeMap:
                     if wall:
-                        render_wall(con, terrain_map, x, y, color_dark_wall, libtcod.BKGND_SET)
+                        render_wall(con, x, y, COLOR_DARK_WALL)
                     else:
-                        libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+                        libtcod.console_set_char_background(con, x, y, COLOR_DARK_GROUND, libtcod.BKGND_SET)
             else:
                 if wall:
-                    render_wall(con, terrain_map, x, y, color_light_wall, libtcod.BKGND_SET)
+                    render_wall(con, x, y, COLOR_LIGHT_WALL)
                 else:
-                    libtcod.console_set_char_background(con, x, y, color_light_ground, libtcod.BKGND_SET)
-                terrain_map[x][y].explored = True
-    for object in objects:
-        object.draw(con, fov_map, debug_show_whole_map)
+                    libtcod.console_set_char_background(con, x, y, COLOR_LIGHT_GROUND, libtcod.BKGND_SET)
+                MapTiles[x][y].explored = True
+    for object in Objects:
+        object.draw(con, fov_map, DebugShowWholeMap)
 
 
-def generate_fov_map(width, height, terrain_map):
+def generate_fov_map(width, height):
     fov_map = libtcod.map_new(width, height)
     for y in range(height):
         for x in range(width):
-            libtcod.map_set_properties(fov_map, x, y, not terrain_map[x][y].block_sight, not terrain_map[x][y].blocked)
+            libtcod.map_set_properties(fov_map, x, y, not MapTiles[x][y].block_sight, not MapTiles[x][y].blocked)
     return fov_map
 
 
 def main():
-    global objects, terrain_map, fov_recompute, player
+    global Objects, MapTiles, FovRecompute, PlayerAction
     libtcod.console_set_custom_font('tiles.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_ASCII_INROW)
     libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Wrath of Exuleb', False)
-    con     = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-    player  = Player(playerx, playery)
-    objects = [player]
-    map_gen = BspMapGenerator(MAP_WIDTH, MAP_HEIGHT, ROOM_MIN_SIZE, BSP_RECURSION_DEPTH, BSP_FULL_ROOMS, max_room_monsters, player)
-    terrain_map = map_gen.generate_map()
-    for o in map_gen.objects:
-        objects.append(o)
-    fov_map = generate_fov_map(MAP_WIDTH, MAP_HEIGHT, terrain_map)
+    con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+    player = Player(PlayerX, PlayerY)
+    Objects = [player]
+    map_gen = BspMapGenerator(MAP_WIDTH, MAP_HEIGHT, ROOM_MIN_SIZE, BSP_RECURSION_DEPTH, BSP_FULL_ROOMS, MAX_ROOM_MONSTERS, player)
+    MapTiles = map_gen.generate_map()
+    for obj in map_gen.objects:
+        Objects.append(obj)
+    fov_map = generate_fov_map(MAP_WIDTH, MAP_HEIGHT)
     while not libtcod.console_is_window_closed():
-        if fov_recompute:
-            fov_recompute = False
+        if FovRecompute:
+            FovRecompute = False
             libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGORITHM)
-        render_all(con, terrain_map, fov_map, objects)
+        render_all(con, fov_map)
         libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
         libtcod.console_flush()
-        for object in objects:
-            object.clear(con)
+        for obj in Objects:
+            obj.clear(con)
         libtcod.console_set_default_foreground(con, libtcod.white)
-        player_action = handle_keys(player)
-        if player_action == 'exit':
+        PlayerAction = handle_keys(player)
+        if PlayerAction == 'exit':
             break
-        if game_state == 'playing' and player_action != 'didnt-take-turn':
-            for o in objects:
-                if o.ai != None:
-                    o.ai.take_turn(fov_map, terrain_map, objects, player)
+        if GameState == 'playing' and PlayerAction != 'didnt-take-turn':
+            for obj in Objects:
+                if obj.ai != None:
+                    obj.ai.take_turn(fov_map, MapTiles, Objects, player)
 
 if __name__ == '__main__':
     main()
